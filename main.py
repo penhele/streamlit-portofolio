f61b203d3166f844b8f584f9bd656dcb3087d8cc
penhele <stephenhelk@gmail.com>
2024-09-27 23:51:33 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import requests 
+
+st.set_page_config(page_title="Portofolio Stephen", page_icon="😁", layout="wide")
+
+# function css
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("cihuy/style/style.css")
+
+with st.container():
+    st.subheader("Hi, Saya Stephen :wave:")
+    st.title("Mahasiswa Gunadarma - Angkatan 2022")
+    st.write("Seorang manusia yang setiap hari duduk di depan laptop dan mengetik sebuah kode.")
+
+with st.container():
+    st.divider()
+    st.header("Tentang Stephen")
+
+    left_column, right_column = st.columns(2)
+
+    with left_column:
+        st.subheader("Universitas Gunadarma")
+        st.write('''
+            - Asisten Laboratorium Komputer (LePKom)
+            - Anggota Komunitas KSPM (Kelompok Studi Pasar Modal)
+            - Finalis Kategori Live Coding Kompetisi Kreativitas Pembuatan Software (KOMPRES 15)
+            - Finalis divisi Competitive Programming yang diadakan Gunadarma I/O
+            - Pecinta Android 
+        ''')
+
+    with right_column:
+        st.subheader('Cerita Singkat')
+        st.write('''
+            Saya adalah mahasiswa aktif Universitas Gunadarma, jurusan Informatika angkatan 2022. 
+            Setelah 2 tahun berkuliah, saya menemukan tujuan saya mengetik kode program selama ini, yaitu berminat karir sebagai Android Developer. 
+            \n\n
+            Saya tidak hanya aktif di kuliah, saya juga aktif dalam komunitas gereja, contohnya menjadi panitia untuk mensukseskan acara besar gereja (Natal, Paskah, dll).
+        ''')
+
+
+with st.container():
+    st.divider()
+
+    st.title('Informasi')
+    st.subheader('Hubungi saya melalui:')
+
+    st.write("""
+        - [Linkedin](https://www.linkedin.com/in/stephen-helenus-ruswanto-kaawoan-09907023b/)
+        - [Instagram](https://www.instagram.com/stephenhelenus)
+    """)
+
+    st.subheader('Terima kasih :wave:')
